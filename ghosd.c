2beee66b8b7c7c90388d848b71291db3083d3cc7
Michal Koutenský <koutak.m@gmail.com>
2018-09-30 16:14:04 +0200

    echo from a fifo

--- ghosd.c
+++ ghosd.c
@@ -1,5 +1,24 @@
+#define GHOSD_FIFO "/tmp/ghosd-fifo"
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+
 int
 main(int argc, char **argv)
 {
+    int fifo;
+    int readfd;
+    unlink(GHOSD_FIFO);
+    fifo   = mkfifo(GHOSD_FIFO, 0644);
+    readfd = open(GHOSD_FIFO, O_RDONLY);
+    char buffer[100];
+    while (1) {
+        if (read(readfd, buffer, 100)) {
+            printf("%s", buffer);
+        }
+    }
     return 0;
 }
